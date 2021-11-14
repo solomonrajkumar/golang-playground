8604bae4f178c777d2d8a05b76ec67a245f9c59c
Rajkumar <solomonrajkumar.as@gmail.com>
2021-11-14 18:17:21 +0530

    Raj | Updates snippet for wait(sync package)

--- lco-playground/26-go-routines/main.go
+++ lco-playground/26-go-routines/main.go
@@ -1,14 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup // usually pointers
 
 func main()  {
-	go greeter("raj")
-	greeter("ron")
-}
-
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		fmt.Println(s)
+	//go greeter("raj")
+	//greeter("ron")
+	websiteList := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://fb.com",
+		"https://github.com",
 	}
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+}
+//
+//func greeter(s string) {
+//	for i := 0; i < 5; i++ {
+//		fmt.Println(s)
+//	}
+//}
+
+func getStatusCode(endpoint string) {
+	defer wg.Done()
+	result, err := http.Get(endpoint)
+	if err != nil {
+		panic(err)
+	}
+	statusCode := result.StatusCode
+	fmt.Printf("%d status code for %s\n", statusCode, endpoint)
 }
